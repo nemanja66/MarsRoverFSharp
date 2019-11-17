62439bb143133bc2e98615ecb860aa30570fd0b9
Nemus <Nemus@DESKTOP-VUL10HB>
2019-11-17 23:27:39 +0100

    Pipe for composition

--- MarsRover/API.fs
+++ MarsRover/API.fs
@@ -6,16 +6,25 @@ open MarsRover
 module Impure = 
     //Gets the persisted position of a rover and maps i to the domain object
     let GetRoverData path = {Position = {x = Coordinate.One; y = Coordinate.One; direction = Direction.North}; Status = Operational; DetectedObstacle = None} 
-    let SaveResult rover path = () //Parses and saves theoutput of a rover movement
+    let SaveResult roverData path = "" //Parses and saves theoutput of a rover movement
     let GetObstacles path = List.empty<Obstacle> //Gets the obstacles
 
  // More pretension
 let path = ""
 
-let Execute: string -> string = 
+//let Execute: string -> string = 
+//    fun commands -> 
+//        let rover = Impure.GetRoverData path 
+//        let obstacles = Impure.GetObstacles path
+//        let result = Execute rover obstacles commands
+//        Impure.SaveResult result path
+//        result
+
+let Execute2: string -> string = 
     fun commands -> 
-        let rover = Impure.GetRoverData path 
-        let obstacles = Impure.GetObstacles path
-        let result = Execute rover obstacles commands
-        Impure.SaveResult result path
-        result
+      Impure.GetRoverData path 
+          |> MarsRover.Execute
+          |> fun execute -> execute (Impure.GetObstacles path)
+          |> fun execute -> execute commands
+          |> Impure.SaveResult path
+
