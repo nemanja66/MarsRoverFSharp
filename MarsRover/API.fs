144b1d3be479f12860264d7ea00442da875f7e25
Nemus <Nemus@DESKTOP-VUL10HB>
2019-11-17 23:07:21 +0100

    Change of composition

--- MarsRover/API.fs
+++ MarsRover/API.fs
@@ -5,8 +5,8 @@ open MarsRover
 // Dirty module that pretends to implement real i.e. filesystem operations
 module Impure = 
     //Gets the persisted position of a rover and maps i to the domain object
-    let GetPosition path = {x = MarsRover.Coordinate.One; y = MarsRover.Coordinate.One; direction = MarsRover.Direction.North} 
-    let SaveResult path rover = () //Parses and saves theoutput of a rover movement
+    let GetRoverData path = {Position = {x = Coordinate.One; y = Coordinate.One; direction = Direction.North}; Status = Operational; DetectedObstacle = None} 
+    let SaveResult rover path = () //Parses and saves theoutput of a rover movement
     let GetObstacles path = List.empty<Obstacle> //Gets the obstacles
 
  // More pretension
@@ -14,12 +14,8 @@ let path = ""
 
 let Execute: string -> string = 
     fun commands -> 
-        let position = Impure.GetPosition path 
+        let rover = Impure.GetRoverData path 
         let obstacles = Impure.GetObstacles path
-        let rover =  {Position = position; Status = Operational; DetectedObstacle = None}
-        let result = MarsRover.Execute rover obstacles commands
-        Impure.SaveResult path result
+        let result = Execute rover obstacles commands
+        Impure.SaveResult result path
         result
-
-
-
