8fe64080bff6c53fbe5020f3af2d088970a68f8f
Nemanja Ljubinkovic <nemanja.ljubinkovic@comdata.rs>
2019-11-19 09:13:39 +0100

    Added comments

--- MarsRover/API.fs
+++ MarsRover/API.fs
@@ -2,25 +2,32 @@
 
 open Domain
 
-// Dirty module that pretends to implement real i.e. filesystem operations
+// This module is created in order to make things more interesting. When the rover receives a set of commands, 
+// it goes to a specific location. When it receives the next set of commands, we want for the rover to continue the movement 
+// from the previous location. This is the reason why we need some kind of persistence.
+// Also, we need to get obstacles from somewhere.
+
+
+// Dirty module that pretends to implement real, for example, filesystem operations
 module Impure = 
     //Gets the persisted position of a rover and maps i to the domain object
     let GetRoverData path = {Position = {x = Coordinate.One; y = Coordinate.One; direction = Direction.North}; Status = Operational; DetectedObstacle = None} 
-    let SaveResult roverData path = () //Parses and saves theoutput of a rover movement
+    let SaveResult roverData path = () //Parses and saves the output of a rover movement
     let GetObstacles path = List.empty<Obstacle> //Gets the obstacles
 
  // More pretension
 let path = ""
 
+// The IMPURE-PURE-IMPURE sandwich
 let Execute: string -> string = 
     fun commands -> 
         let rover = Impure.GetRoverData path 
         let obstacles = Impure.GetObstacles path
-        let result = Execute rover obstacles commands
+        let result = Execute rover obstacles commands //Pure
         Impure.SaveResult result path
         result
 
-//Exeucte with pipe
+//Exeucte with pipe attempt
 //let SaveResult roverData path = "" //Parses and saves theoutput of a rover movement
 //let Execute: string -> string = 
 //    fun commands -> 
